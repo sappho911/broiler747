8587092c71fec6121ff19656e3f66a8318b382f0
Aleksandr Kuzmich <58181433+sappho911@users.noreply.github.com>
2025-12-02 13:52:23 +0200

    Delete src directory

--- src/config/db_config.py
+++
@@ -1,8 +0,0 @@
-DB_CONFIG = {
-    "host": "127.0.0.1",
-    "port": 3306,
-    "database": "flight_game",
-    "user": "valera",
-    "password": "loh",
-    "autocommit": True
-}
--- src/views/view.py
+++
@@ -1,3 +0,0 @@
-class Airport:
-    def __init__(self, name):
-        self.name = name
